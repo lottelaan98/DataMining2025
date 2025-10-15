10f5274d13038b2a979b577161884759170ebe3d
lottelaan98 <99404774+lottelaan98@users.noreply.github.com>
2025-10-15 23:55:41 +0200

    Update Logistic regression with Lasso penalty.py

--- Logistic regression with Lasso penalty.py
+++ Logistic regression with Lasso penalty.py
@@ -17,22 +17,17 @@ class TrainModel2:
     @staticmethod
     def train_model(train_df, test_df, vectorizer):
         # Create a pipeline that first vectorizes the text data and then applies the Logistic Regression with Lasso penalty
-        model = make_pipeline(vectorizer, LogisticRegression(penalty='l1', solver='saga', max_iter=1000))
-
-        # train_df = Word_preprocessing.preprocess_text(train_df['content'])
-        # test_df = Word_preprocessing.preprocess_text(test_df['content'])
+        model = make_pipeline(vectorizer, LogisticRegression(penalty='l1', solver='saga', max_iter=5000))
 
         # Train the model using the training data
         model.fit(train_df['content'], train_df['label'])
 
-        evaluate_model(model, test_df["content"], test_df["label"])
-
         return model
 
     @staticmethod
-    def hyperparameter_tuning(train_df, vectorizer, vectorizer_ngram):
+    def hyperparameter_tuning(train_df, test_df, vectorizer, vectorizer_ngram):
         # Create a pipeline that first vectorizes the text data and then applies the Logistic Regression with Lasso penalty
-        model = make_pipeline(vectorizer, LogisticRegression(penalty='l1', solver='saga', max_iter=1000))
+        model = make_pipeline(vectorizer, LogisticRegression(penalty='l1', solver='saga', max_iter=5000))
 
         # Define the parameter grid to search
         param_grid = {
@@ -41,18 +36,10 @@ class TrainModel2:
         }
 
         # Set up GridSearchCV
-        grid_search = GridSearchCV(model, param_grid, cv=5, n_jobs=-1, verbose=1)
+        grid_search = GridSearchCV(model, param_grid, cv=5, n_jobs=-1, verbose=0)
 
         # Perform the grid search on the training data
         grid_search.fit(train_df['content'], train_df['label'])
-
-        # Print the best parameters found by GridSearchCV
-        print("Best Parameters:", grid_search.best_params_)
-
-        # Evaluate the best model on the test set
-        best_model = grid_search.best_estimator_
-        
-        evaluate_model(best_model, test_df["content"], test_df["label"])
 
         return grid_search.best_estimator_
     
@@ -74,28 +61,38 @@ class TrainModel2:
         print("\nTop negative words:")
         for i in top_neg_idx:
             print(f"{feature_names[i]}: {coefs[i]:.3f}")
+            
+    def accuracyvalue(model, X_test, y_test):
 
-    
-# But by importing OenFiles, I can still use the load_files_recursive function in another script
-if __name__ == "__main__":
+        y_pred = model.predict(X_test)
+        
+        return accuracy_score(y_test, y_pred)
 
-    train_df, test_df = Split_data.split_data(train=(1,2,3,4), test=(5,))
+def Output_Logistic(train_df, test_df):
 
     models = [
         ("The countvectorizer model", "The tuned countvectorizer model", CountVectorizer(), "countvectorizer__ngram_range", "countvectorizer"),
         ("The countervectorizer model with preprocessing ", "The tuned countvectorizer model with preprocessing ", CountVectorizer(lowercase=True, stop_words="english"), "countvectorizer__ngram_range", "countvectorizer"),
         ("The tfifdvectorizer model ", "The tuned tfifdvectorizer model ", TfidfVectorizer(), 'tfidfvectorizer__ngram_range', "tfidfvectorizer"),
-        ("The countervectorizer model with preprocessing ", "The tuned countervectorizer model with preprocessing ", TfidfVectorizer(lowercase=True, stop_words="english"), 'tfidfvectorizer__ngram_range', "tfidfvectorizer")
+        ("The tfifdvectorizer model with preprocessing ", "The tuned tfifdvectorizer model with preprocessing ", TfidfVectorizer(lowercase=True, stop_words="english"), 'tfidfvectorizer__ngram_range', "tfidfvectorizer")
     ]
+
+    accuracy = []
     
     for i in models:
-        # Train the model and then tune the model
-        print(i[0],", and without hyperparameter tuning")
         model = TrainModel2.train_model(train_df, test_df, i[2])
-        print(i[1])
-        best_model = TrainModel2.hyperparameter_tuning(train_df, i[2], i[3])
-        # Get feature importance
-        print("Feature importance of ", i[0])
-        TrainModel2.give_feature_importance(model, i[4])
-        print("Feature importance of ", i[1])
-        TrainModel2.give_feature_importance(best_model, i[4])
+        accuracy.append([i[0], TrainModel2.accuracyvalue(model, test_df["content"], test_df["label"]), model, i[4]])
+        best_model = TrainModel2.hyperparameter_tuning(train_df, test_df, i[2], i[3])
+        accuracy.append([i[1], TrainModel2.accuracyvalue(best_model, test_df["content"], test_df["label"]), best_model, i[4]])
+
+    best_model = []
+    highest_acc = 0
+
+    for highest in accuracy:
+        if highest[1] > highest_acc:
+            highest_acc = highest[1]
+            best_model = highest
+    
+    print("The best model is:", best_model[:2])
+    evaluate_model(best_model[2], test_df["content"], test_df["label"])
+    TrainModel2.give_feature_importance(best_model[2], best_model[3])
